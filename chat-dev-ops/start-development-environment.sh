e43e7892713e0fa4b3945dc7cf04cf45775268f7
Kevin <kevin.denver@gmail.com>
2017-06-11 16:31:38 +0100

    Added docker env file support

--- chat-dev-ops/start-development-environment.sh
+++ chat-dev-ops/start-development-environment.sh
@@ -17,39 +17,32 @@ generate_gitlab_secrets() {
     GITLAB_SECRETS_DB_KEY_BASE=$(docker run --rm kciepluc/pwgen-docker -B -s 128)
     GITLAB_SECRETS_SECRET_KEY_BASE=$(docker run --rm kciepluc/pwgen-docker -B -s 128)
     GITLAB_SECRETS_OTP_KEY_BASE=$(docker run --rm kciepluc/pwgen-docker -B -s 128)
-    touch ./data/gitlab_secrets.sh && chmod u+x ./data/gitlab_secrets.sh
-    cat > ./data/gitlab_secrets.sh <<EOL
-#!/bin/sh
-set -u
-set -e
-echo export GITLAB_SECRETS_DB_KEY_BASE=${GITLAB_SECRETS_DB_KEY_BASE}
-echo export GITLAB_SECRETS_SECRET_KEY_BASE=${GITLAB_SECRETS_SECRET_KEY_BASE}
-echo export GITLAB_SECRETS_OTP_KEY_BASE=${GITLAB_SECRETS_OTP_KEY_BASE}
+    cat > ./.env <<EOL
+GITLAB_SECRETS_DB_KEY_BASE=${GITLAB_SECRETS_DB_KEY_BASE}
+GITLAB_SECRETS_SECRET_KEY_BASE=${GITLAB_SECRETS_SECRET_KEY_BASE}
+GITLAB_SECRETS_OTP_KEY_BASE=${GITLAB_SECRETS_OTP_KEY_BASE}
 EOL
 }
 
 set_concourse_external_url() {
-    export CONCOURSE_EXTERNAL_URL=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | head -1)
+    CONCOURSE_EXTERNAL_URL=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | head -1)
+    echo "CONCOURSE_EXTERNAL_URL=${CONCOURSE_EXTERNAL_URL}" >> ./.env
 }
 
 if [ ! -f ./data/concourse/worker/worker_key.pub ]
 then
-    echo "Generating keys for Concourse for the first time..."
-    generate_concourse_keys
+    echo "Generating keys for Concourse for the first time..." && generate_concourse_keys
 else 
     echo "Found existing Concourse keys. Skipping key generation"
 fi
 
-if [ ! -f ./data/gitlab_secrets.sh ]
+if [ ! -f ./.env ]
 then
-    echo "Generating GitLab secrets for the first time..."
-    generate_gitlab_secrets
+    echo "Generating GitLab secrets for the first time..." && generate_gitlab_secrets
+    set_concourse_external_url && echo "Setting CONCOURSE_EXTERNAL_URL to ${CONCOURSE_EXTERNAL_URL}"
 else 
     echo "Found existing GitLab keys. Skipping key generation"
-    eval $(./data/gitlab_secrets.sh)
 fi
-
-set_concourse_external_url && echo "Setting CONCOURSE_EXTERNAL_URL to ${CONCOURSE_EXTERNAL_URL}"
 
 docker-compose up
 # docker-compose start
